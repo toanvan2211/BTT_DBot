d8e72854603ebe4b7fe3b74ce834e452f6478abb
Văn Công Toàn <vanc.t123@gmail.com>
2020-11-18 00:05:58 +0700

    first version

---
+++ BotDiscord/Modules/Commands.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace BotDiscord.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        public async Task Ping()
+        {
+            await ReplyAsync("Pong");
+        }
+
+        [Command("pong")]
+        public async Task Pong()
+        {
+            await ReplyAsync("Haha");
+        }
+    }
+}
---
+++ DBot/Commands/FunCommands.cs
@@ -0,0 +1,37 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBot.Commands
+{
+    public class FunCommands : BaseCommandModule
+    {
+        [Command("ping")]
+        [Description("Pong pong pong!!!")]
+        public async Task Ping(CommandContext ctx)
+        {
+            await ctx.Channel.SendMessageAsync("Pong").ConfigureAwait(false);
+        }
+
+        [Command("Giỏi")]
+        [RequireRoles(RoleCheckMode.All, "Moderator")]
+        public async Task ThanksSir(CommandContext ctx)
+        {
+            await ctx.Channel.SendMessageAsync("Dạ cám ơn ông chủ").ConfigureAwait(false);
+        }
+
+        [Command("add")]
+        [Description("Cộng hai số nguyên")]
+        public async Task Add(CommandContext ctx, 
+            [Description("Số nguyên thứ nhất")] int numberOne,
+            [Description("Số nguyên thứ hai")] int numberTwo)
+        {
+            await ctx.Channel
+                .SendMessageAsync((numberOne + numberTwo).ToString())
+                .ConfigureAwait(false);
+        }
+    }
+}
